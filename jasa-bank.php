428dee33f9d972133d36147ba15372b4d62f792f
Buhairi <79437567+buhairi-dev@users.noreply.github.com>
2021-05-09 14:51:26 +0700

    Jasa Bank

---
+++ jasa-bank.php
@@ -0,0 +1,109 @@
+<?php include 'layout/header.php';?>
+
+<section id="img-header" class="img-header">
+<img src="asset/slider25.png">
+<ul class="breadcrumb">
+<div class="container">
+  <li><a href="#">Home</a></li>
+  <li>Bisnins</li>
+  <li>Jasa Bank</li>
+</div>
+</ul>
+</section>
+
+
+
+<section class="section">
+  <div class="container">
+    <div class="row">
+    <div class="col-sm-5">
+    <br/> <br/>
+    <h4 class="mt-5">Tarif Layanan</h4>
+    <small>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet</small>
+    <br/> <br/>
+    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="#">Selengkapnya</a></p>
+    </div>
+
+    <div class="col-sm-5">
+    <img src="asset/tarif-layanan.png"  alt="Logo" width="600px" style="margin-left:20%;height: 350px; margin-bottom:8%;">
+    </div>
+    </div>
+  </div>
+  <br/>
+
+  <div class="container">
+    <div class="row">
+    <div class="col-sm-7">
+    <img src="asset/bank-garansi.png"  alt="Logo" width="600px" style="height: 350px; margin-bottom:8%;">
+    </div>
+
+    <div class="col-sm-5">
+    <br/> <br/>
+    <h4 class="mt-5">Bank Garansi</h4>
+    <small>Salah satu hal penting dalam bisnis adalah kepercayaan. Namun, kepercayaan biasanya baru datang bila rekam jejak para pihak yang bertransaksi dapat diketahui atau bila ada pihak yang didukung atau dijamin oleh pihak ketiga yang dapat dipercaya.</small>
+    <br/> <br/>
+    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="#">Berita Mantap</a></p>
+    </div>
+    </div>
+  </div>
+  <br/>
+
+  <div class="container">
+    <div class="row">
+    <div class="col-sm-5">
+    <br/> <br/>
+    <h4 class="mt-5">Referensi Bank</h4>
+    <small>Salah satu hal penting dalam bisnis adalah kepercayaan. Namun, kepercayaan biasanya baru datang bila rekam jejak para pihak yang bertransaksi dapat diketahui atau bila ada pihak yang didukung oleh pihak ketiga yang dapat dipercaya.</small>
+    <br/> <br/>
+    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="#">Selengkapnya</a></p>
+    </div>
+
+    <div class="col-sm-5">
+    <img src="asset/referensi-bank.png"  alt="Logo" width="600px" style="margin-left:20%;height: 350px; margin-bottom:8%;">
+    </div>
+    </div>
+  </div>
+  <br/>
+
+  <div class="container">
+    <div class="row">
+    <div class="col-sm-7">
+    <img src="asset/pic-transfer.png"  alt="Logo" width="600px" style="height: 350px; margin-bottom:8%;">
+    </div>
+
+    <div class="col-sm-5">
+    <br/> <br/>
+    <h4 class="mt-5">Transfer</h4>
+    <small>Jasa pengiriman uang yang dilaksanakan atas permintaan dan untuk kepentingan nasabah.</small>
+    <br/> <br/>
+    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="#">Selengkapnya</a></p>
+    </div>
+    </div>
+  </div>
+
+ <br/>
+  <div class="container">
+    <div class="row">
+    <div class="col-sm-5">
+    <br/> <br/>
+    <h4 class="mt-5">Inkaso</h4>
+    <small>Layanan Bank Mantap dalam rangka penagihan warkat-warkat valuta rupiah yang Bank tertariknya berada diluar wilayah Kliring.</small>
+    <br/> <br/>
+    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="#">Selengkapnya</a></p>
+    </div>
+
+    <div class="col-sm-5">
+    <img src="asset/pic-inkaso.png"  alt="Logo" width="550px" style="margin-left:20%;height: 350px; margin-bottom:8%;">
+    </div>
+    </div>
+  </div>
+ 
+ 
+
+
+</section>
+
+ 
+</div>
+
+<?php include 'layout/footer.php';?>
