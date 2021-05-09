16e37615d577863c21837e98a92d472d4948aeaf
Buhairi <79437567+buhairi-dev@users.noreply.github.com>
2021-05-09 14:50:51 +0700

    Simpanan

---
+++ simpanan.php
@@ -0,0 +1,121 @@
+<?php include 'layout/header.php';?>
+
+<section id="img-header" class="img-header">
+<img src="asset/slider24.png">
+<ul class="breadcrumb">
+<div class="container">
+  <li><a href="#">Home</a></li>
+  <li>Bisnins</li>
+  <li>Simpanan Mantap</li>
+</div>
+</ul>
+</section>
+
+
+
+<section class="section">
+  <div class="container">
+    <div class="row">
+    <div class="col-sm-5">
+    <br/> <br/>
+    <h4 class="mt-5">Simpanan Tabunganku</h4>
+    <small>TabunganKu adalah tabungan untuk perorangan Warga Negara Indonesia dengan persyaratan mudah dan ringan yang diterbitkan secara bersama oleh perbankan di Indonesia</small>
+    <br/> <br/>
+    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="#">Selengkapnya</a></p>
+    </div>
+
+    <div class="col-sm-5">
+    <img src="asset/img-tabunganku.png"  alt="Logo" width="600px" style="margin-left:20%;height: 350px; margin-bottom:8%;">
+    </div>
+    </div>
+  </div>
+ 
+  <div class="container">
+    <div class="row">
+    <div class="col-sm-7">
+    <img src="asset/img-simulasi01.png"  alt="Logo" width="600px" style="height: 350px; margin-bottom:8%;">
+    </div>
+
+    <div class="col-sm-5">
+    <br/> <br/>
+    <h4 class="mt-5">Tabungan Simantap Berjangka</h4>
+    <small>Tabungan Simantap Berjangka (TSB) adalah tabungan dengan setoran wajib bulanan yang memberikan kesempatan kepada anda untuk mempersiapkan masa depan yang lebih baik.</small>
+    <br/> <br/>
+    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="#">Selengkapnya</a></p>
+    </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+    <div class="col-sm-5">
+    <br/> <br/>
+    <h4 class="mt-5">Tabungan siMantap</h4>
+    <small>Tabungan SiMantap Gold adalah tabungan untuk perorangan Warga Negara Indonesia dengan persyaratan mudah dan ringan</small>
+    <br/> <br/>
+    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="#">Selengkapnya</a></p>
+    </div>
+
+    <div class="col-sm-5">
+    <img src="asset/1519090948.png"  alt="Logo" width="600px" style="margin-left:20%;height: 350px; margin-bottom:8%;">
+    </div>
+    </div>
+  </div>
+ 
+  <div class="container">
+    <div class="row">
+    <div class="col-sm-7">
+    <img src="asset/pinjaman3.png"  alt="Logo" width="600px" style="height: 350px; margin-bottom:8%;">
+    </div>
+
+    <div class="col-sm-5">
+    <br/> <br/>
+    <h4 class="mt-5">Tabungan siMantap Pensiun</h4>
+    <small>Tabungan SiMantap Pensiun adalah tabungan untuk nasabah pensiun sebagai sarana untuk menampung uang pensiun dan gaji pensiun setiap bulannya atas penunjukan Bank Mantap sebagai Bank juru bayar gaji pensiun.</small>
+    <br/> <br/>
+    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="#">Selengkapnya</a></p>
+    </div>
+    </div>
+  </div>
+
+ 
+  <div class="container">
+    <div class="row">
+    <div class="col-sm-5">
+    <br/> <br/>
+    <h4 class="mt-5">Deposito Mantap</h4>
+    <small>Deposito Bank Mantap Nikmati<br/>kemudahan bertransaksi dengan persyaratan mudah dan ringan</small>
+    <br/> <br/>
+    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="#">Selengkapnya</a></p>
+    </div>
+
+    <div class="col-sm-5">
+    <img src="asset/img-simulasi02.png"  alt="Logo" width="550px" style="margin-left:20%;height: 350px; margin-bottom:8%;">
+    </div>
+    </div>
+  </div>
+ 
+  <div class="container">
+    <div class="row">
+    <div class="col-sm-7">
+    <img src="asset/img-giro.png"  alt="Logo" width="600px" style="height: 350px; margin-bottom:8%;">
+    </div>
+
+    <div class="col-sm-5">
+    <br/> <br/>
+    <h4 class="mt-5">Giro</h4>
+    <small>Di tengah tantangan bisnis saat ini, Anda tentu membutuhkan berbagai sarana pendukung yang terpercaya. Untuk itulah Bank Mantap menawarkan transaksi perbankan menguntungkan melalui Giro Bank Mantap.</small>
+    <br/> <br/>
+    <p><a class="btn btn-sm btn-yellow-sm" role="button" href="#">Berita Mantap</a></p>
+    </div>
+    </div>
+  </div>
+
+
+
+</section>
+
+ 
+</div>
+
+<?php include 'layout/footer.php';?>
